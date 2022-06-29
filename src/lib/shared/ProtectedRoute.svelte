d3899c6e9db6f96678deb7a7cb3469d97584e699
Andres David Jimenez <andresdavid@outlook.com>
2022-06-29 10:35:13 -0500

    clean

---
+++ src/lib/shared/ProtectedRoute.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { Route } from 'svelte-routing';
+
+    export let path: string;
+    export let component: any;
+</script>
+
+<Route path={path} component={component} />
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
+	import Header from '$lib/shared/header/Header.svelte';
 	import '../app.css';
 </script>
 
