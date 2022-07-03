f74d2f3d35dda6dee046d986db2c3cc25fd69564
Andres David Jimenez <andresdavid@outlook.com>
2022-07-03 18:42:24 -0500

    try netilfy

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,18 @@
 // import adapter from '@sveltejs/adapter-auto';
 // import expressAdapter from "@mankins/svelte-adapter-express"
-import vercel from '@sveltejs/adapter-vercel';
+// import vercel from '@sveltejs/adapter-vercel';
+// import node from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-	}),
+	preprocess: adapter(),
 
 	kit: {
-		adapter: vercel({
-			edge: false,
-			split: false
-		}),
+		adapter: node({ env: { port: process.env.PORT } }),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
