28946681654c4a430c90458bdbdff511ae8a86d7
Andres David Jimenez <andresdavid@outlook.com>
2022-07-03 19:12:03 -0500

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,10 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: false,
+			split: true
+		}),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
