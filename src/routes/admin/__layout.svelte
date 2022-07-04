95207081cca7cc2251cb953b9f5c101dc59649e0
Andres David Jimenez <andresdavid@outlook.com>
2022-07-04 10:35:46 -0500

    adding husky

--- src/routes/admin/__layout.svelte
+++ src/routes/admin/__layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-import Drawer from '$lib/shared/drawer/drawer.svelte';
-import '../../app.css';
+	import Drawer from '$lib/shared/drawer/drawer.svelte';
+	import '../../app.css';
 </script>
+
+<svelete:head>
+	<title>Admin</title>
+</svelete:head>
 <Drawer>
-      <!-- Page content here -->
-      <slot />
+	<!-- Page content here -->
+	<slot />
 </Drawer>
